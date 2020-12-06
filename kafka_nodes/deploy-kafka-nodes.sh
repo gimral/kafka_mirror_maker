e69f980c64f70be28ebd9e5ba137713e894ba3df
gokhanimral <gimral@gmail.com>
2020-12-06 14:41:00 +0400

    mm2 deploy

--- kafka_nodes/deploy-kafka-nodes.sh
+++ kafka_nodes/deploy-kafka-nodes.sh
@@ -1,3 +1,5 @@
+docker stack rm kafka
+sleep 20s
 docker stack deploy -c source-kafka/zookeeper.yaml kafka 
 docker stack deploy -c source-kafka/kafka.yaml kafka
 docker stack deploy -c target-kafka/zookeeper.yaml kafka
--- mirror_makers/deploy-mmv2.sh
+++ mirror_makers/deploy-mmv2.sh
@@ -1,4 +1,7 @@
-docker service rm kafka_mmv2
-docker config rm kafka_mm2.properties
+docker service rm kafka_source_mmv2
+docker config rm kafka_source_mm2.properties
+docker service rm kafka_target_mmv2
+docker config rm kafka_target_mm2.properties
 sleep 3s
-docker stack deploy -c mmv2.yaml kafka
+docker stack deploy -c source_mmv2.yaml kafka
+docker stack deploy -c target_mmv2.yaml kafka
